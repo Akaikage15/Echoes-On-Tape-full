951563a18d0ded9601500f7ad7dbdeb0f5e2f835
Akaikage15 <knecuchrin@ya.ru>
2025-11-21 18:00:23 +0500

    fix: исправлен экспорт репозиториев в index.ts
    - Добавлены явные импорты и экспорты экземпляров
    - Исправлена ошибка TypeScript с demoRepository и другими
    - Все репозитории теперь корректно экспортируются

--- backend/src/repositories/index.ts
+++ backend/src/repositories/index.ts
@@ -3,6 +3,17 @@
  * Экспорт всех репозиториев
  */
 
+import { userRepository } from './user.repository';
+import { artistRepository } from './artist.repository';
+import { releaseRepository } from './release.repository';
+import { postRepository } from './post.repository';
+import { exclusiveRepository } from './exclusive.repository';
+import { merchRepository } from './merch.repository';
+import { pollRepository } from './poll.repository';
+import { proLibraryRepository } from './pro-library.repository';
+import { demoRepository } from './demo.repository';
+
+// Экспорт классов и типов
 export * from './user.repository';
 export * from './artist.repository';
 export * from './release.repository';
@@ -12,3 +23,16 @@ export * from './merch.repository';
 export * from './poll.repository';
 export * from './pro-library.repository';
 export * from './demo.repository';
+
+// Экспорт экземпляров
+export {
+  userRepository,
+  artistRepository,
+  releaseRepository,
+  postRepository,
+  exclusiveRepository,
+  merchRepository,
+  pollRepository,
+  proLibraryRepository,
+  demoRepository,
+};
